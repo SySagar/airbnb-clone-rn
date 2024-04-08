c29d9da446809899734a9239952b2edb0f58e336
SySagar <sysagar07@gmail.com>
2024-04-09 00:41:37 +0530

    feat: search header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,29 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Link } from "expo-router";
+import React, { useMemo } from "react";
+import { Stack } from "expo-router";
+import ExploreHeader from "@/components/ExploreHeader";
+import Listings from "@/components/Listings";
+import listingData from "@/assets/data/airbnb-listings.json";
 
 const Page = () => {
+
+  const [category, setCategory] = React.useState<string>("Tiny Homes" as string);
+  const items = useMemo(() => listingData as any,[])
+  const onDataChanged = (category:string)=>{
+    setCategory(category);
+  }
+
   return (
-    <View>
-      <Link href={"/(modals)/login"}>Login</Link>
-      
-      <Link href={"/(modals)/booking"}>Booking</Link>
-      
-      <Link href={"/listing/156"}>Listing details</Link>
+    <View style={{flex:1,marginTop:140}}>
+      <Stack.Screen 
+      options={{
+        header: () => <ExploreHeader onCategoryChanged={onDataChanged} />
+      }}
+      />
+      <Listings 
+      listings={items}
+      category={category}
+      />
     </View>
   );
 };
